85b669e67f599eb9eb52c8f6a69f8afc159a5513
vinaypuppal <me@vinaypuppal.com>
2021-04-27 17:35:38 +0530

    update help section

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,8 @@ sade("rapidsec")
   .describe(`Adds security headers to _headers file using RapidSec Service`)
   .option(`-i, --input`, `Provide path to existing _headers file`)
   .option(`-o, --output`, `Provide path to output _headers file`)
-  .example(`netlify -o ./dist`)
-  .example(`netlify -i ./public/_headers -o ./dist`)
+  .example(`netlify -o ./dist/_headers`)
+  .example(`netlify -i ./public/_headers -o ./dist/_headers`)
   .action((options) => {
     provider.handleProvider(options, Providers.Netlify);
   })
@@ -22,8 +22,8 @@ sade("rapidsec")
   .describe(`Adds security headers to vercel.json file using RapidSec Service`)
   .option(`-i, --input`, `Provide path to existing vercel.json file`)
   .option(`-o, --output`, `Provide path to output vercel.json file`)
-  .example(`netlify -o ./out`)
-  .example(`netlify -i ./public/vercel.json -o ./out`)
+  .example(`netlify -o ./out/vercel.json`)
+  .example(`netlify -i ./public/vercel.json -o ./out/vercel.json`)
   .action((options) => {
     provider.handleProvider(options, Providers.Vercel);
   })
@@ -31,8 +31,8 @@ sade("rapidsec")
   .describe(`Adds security headers to vercel.json file using RapidSec Service`)
   .option(`-i, --input`, `Provide path to existing vercel.json file`)
   .option(`-o, --output`, `Provide path to output vercel.json file`)
-  .example(`firebase -o ./out`)
-  .example(`firebase -i ./firebase.json -o ./out`)
+  .example(`firebase -o ./out/firebase.json`)
+  .example(`firebase -i ./firebase.json -o ./out/firebase.json`)
   .action((options) => {
     provider.handleProvider(options, Providers.Firebase);
   })
